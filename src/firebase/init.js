ce751d90acae6747c28548f1ec25c9bd28e3e2dc
LittleElijah <cuiyeqing960904@gmail.com>
2018-07-14 19:30:46 +0300

    firebase set up

---
+++ src/firebase/init.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+var config = {
+    apiKey: "AIzaSyAegq8omGFF5aZwFGx8VvhnIIOQGsTCG5I",
+    authDomain: "vuefs-prod-36747.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-36747.firebaseio.com",
+    projectId: "vuefs-prod-36747",
+    storageBucket: "vuefs-prod-36747.appspot.com",
+    messagingSenderId: "850166238486"
+  };
+ const firebaseApp = firebase.initializeApp(config);
+
+ export default firebaseApp.firestore()
